361ca470799171e2dbb8f465cd4aa96e7df201dc
Saloni Atole <145243379+Saloni1707@users.noreply.github.com>
2024-08-08 23:35:42 +0530

    Update First_lastOcc.cpp
    Here another approach has been added

--- Binary Search/First_lastOcc.cpp
+++ Binary Search/First_lastOcc.cpp
@@ -21,6 +21,23 @@ int FirstOccurence(vector<int> arr , int n , int x){
     }
     return ans ; 
 }
+//found another approach here 
+vector<int> res(2,-1); //here a vector res has been initialised with size 2 & initialized with both elements as -1. This vector will store the first and last indices of x.
+        for(int i = 0; i < n; i++){
+            if(arr[i] == x){
+                res[0] = i;    //stores the first index
+                break;
+            }
+        }
+        for(int i = n-1; i >= 0; i--){
+            if(arr[i] == x){
+                res[1] = i;    //stores the last index 
+                break;
+            }
+        }
+        
+        return res;
+
 int  main(){
     vector<int> a ={1,2,3,3,3,6};
     int n = 6 , x = 3 ;
